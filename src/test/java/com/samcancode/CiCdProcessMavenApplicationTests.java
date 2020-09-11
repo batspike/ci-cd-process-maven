f893b6780a3c3d39e2d33634ecde6fba65038e11
Sam Lau <samlau.sam@gmail.com>
2020-09-11 22:26:54 +0800

    added main app test for coverage

--- src/test/java/com/samcancode/CiCdProcessMavenApplicationTests.java
+++ src/test/java/com/samcancode/CiCdProcessMavenApplicationTests.java
@@ -9,5 +9,10 @@ class CiCdProcessMavenApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void applicationContextTest() {
+		CiCdProcessMavenApplication.main(new String[] {});
+	}
 
 }
